f42ed53e4ac873e8434332cec86fae019635f9f8
ff <1025019486@qq.com>
2016-09-26 21:07:44 +0800

    testusnity

---
+++ test/Assets/theClass.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class theClass  {
+
+	public  int num=0;
+	private static theClass _instance;
+	public static theClass getInstance(){
+
+		if (_instance == null) {
+		
+			_instance = new theClass ();
+		}
+		return _instance;
+	}
+
+}
